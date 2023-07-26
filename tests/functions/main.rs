c42f0c207edc97bfa97fcbf48c297652e19d31a5
Martin Fischer <martin@push-f.com>
2023-07-26 21:03:42 +0200

    build: build integration tests in a single binary (#40)

---
+++ tests/functions/main.rs
@@ -0,0 +1,18 @@
+// We have these integration tests in a single binary, so that they compile much faster.
+
+mod arrange;
+mod count;
+mod df_var;
+mod distinct;
+mod filter;
+mod glimpse;
+mod group_by;
+mod head;
+mod join;
+mod json;
+mod mutate;
+mod relocate;
+mod rename;
+mod select;
+mod show;
+mod unnest;
